652d658383056a9866d8c7fd7da83c22844eba2c
Janito Vaqueiro Ferreira Filho <janito@mullvad.net>
2020-12-02 18:16:59 +0000

    Use intermittent daemon in `LocationInfoCache`

--- android/src/main/kotlin/net/mullvad/mullvadvpn/service/ServiceInstance.kt
+++ android/src/main/kotlin/net/mullvad/mullvadvpn/service/ServiceInstance.kt
@@ -3,11 +3,13 @@ package net.mullvad.mullvadvpn.service
 import android.os.Messenger
 import net.mullvad.mullvadvpn.service.endpoint.AccountCache
 import net.mullvad.mullvadvpn.service.endpoint.SettingsListener
+import net.mullvad.mullvadvpn.util.Intermittent
 import net.mullvad.talpid.ConnectivityListener
 
 class ServiceInstance(
     val messenger: Messenger,
     val daemon: MullvadDaemon,
+    val intermittentDaemon: Intermittent<MullvadDaemon>,
     val connectionProxy: ConnectionProxy,
     val connectivityListener: ConnectivityListener,
     val customDns: CustomDns,
@@ -16,7 +18,9 @@ class ServiceInstance(
 ) {
     val accountCache = AccountCache(daemon, settingsListener)
     val keyStatusListener = KeyStatusListener(daemon)
-    val locationInfoCache = LocationInfoCache(daemon, connectionProxy, connectivityListener)
+
+    val locationInfoCache =
+        LocationInfoCache(connectionProxy, connectivityListener, intermittentDaemon)
 
     fun onDestroy() {
         accountCache.onDestroy()
