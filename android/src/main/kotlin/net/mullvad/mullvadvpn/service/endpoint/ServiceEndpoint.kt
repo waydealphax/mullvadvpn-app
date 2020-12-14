7fba272b0399b6ef1aef8df282935be47ebd9683
Janito Vaqueiro Ferreira Filho <janito@mullvad.net>
2020-12-14 20:10:06 +0000

    Send settings update events

--- android/src/main/kotlin/net/mullvad/mullvadvpn/service/endpoint/ServiceEndpoint.kt
+++ android/src/main/kotlin/net/mullvad/mullvadvpn/service/endpoint/ServiceEndpoint.kt
@@ -15,7 +15,10 @@ import net.mullvad.mullvadvpn.ipc.Request
 import net.mullvad.mullvadvpn.service.MullvadDaemon
 import net.mullvad.mullvadvpn.util.Intermittent
 
-class ServiceEndpoint(looper: Looper, private val intermittentDaemon: Intermittent<MullvadDaemon>) {
+class ServiceEndpoint(
+    looper: Looper,
+    internal val intermittentDaemon: Intermittent<MullvadDaemon>
+) {
     private val listeners = mutableListOf<Messenger>()
     private val registrationQueue = startRegistrator()
 
@@ -25,7 +28,7 @@ class ServiceEndpoint(looper: Looper, private val intermittentDaemon: Intermitte
 
     val messenger = Messenger(dispatcher)
 
-    val settingsListener = SettingsListener(intermittentDaemon)
+    val settingsListener = SettingsListener(this)
 
     init {
         dispatcher.registerHandler(Request.RegisterListener::class) { request ->
