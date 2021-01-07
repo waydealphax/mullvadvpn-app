edf1f071be5314fdcab75f16ab9afa6ff200958e
Janito Vaqueiro Ferreira Filho <janito@mullvad.net>
2021-01-07 17:56:32 +0000

    Handle new relay list events in the UI side

--- android/src/main/kotlin/net/mullvad/mullvadvpn/ui/serviceconnection/ServiceConnection.kt
+++ android/src/main/kotlin/net/mullvad/mullvadvpn/ui/serviceconnection/ServiceConnection.kt
@@ -24,7 +24,7 @@ class ServiceConnection(private val service: ServiceInstance) {
 
     val appVersionInfoCache = AppVersionInfoCache(dispatcher, settingsListener)
     val customDns = CustomDns(service.messenger, settingsListener)
-    var relayListListener = RelayListListener(daemon, settingsListener)
+    var relayListListener = RelayListListener(dispatcher, settingsListener)
 
     init {
         registerListener()
