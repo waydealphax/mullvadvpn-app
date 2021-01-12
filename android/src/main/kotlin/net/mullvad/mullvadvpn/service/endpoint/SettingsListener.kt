85c3fb42c13767721bbfd7f1af0e29c343aba41d
Janito Vaqueiro Ferreira Filho <janito@mullvad.net>
2021-01-12 14:32:33 +0000

    Allow to set WireGuard MTU with `SettingsListener`

--- android/src/main/kotlin/net/mullvad/mullvadvpn/service/endpoint/SettingsListener.kt
+++ android/src/main/kotlin/net/mullvad/mullvadvpn/service/endpoint/SettingsListener.kt
@@ -1,12 +1,24 @@
 package net.mullvad.mullvadvpn.service.endpoint
 
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.channels.ClosedReceiveChannelException
+import kotlinx.coroutines.channels.actor
+import kotlinx.coroutines.channels.sendBlocking
 import net.mullvad.mullvadvpn.ipc.Event
+import net.mullvad.mullvadvpn.ipc.Request
 import net.mullvad.mullvadvpn.model.DnsOptions
 import net.mullvad.mullvadvpn.model.RelaySettings
 import net.mullvad.mullvadvpn.model.Settings
 import net.mullvad.talpid.util.EventNotifier
 
 class SettingsListener(endpoint: ServiceEndpoint) {
+    private sealed class Command {
+        class SetWireGuardMtu(val mtu: Int?) : Command()
+    }
+
+    private val commandChannel = spawnActor()
     private val daemon = endpoint.intermittentDaemon
 
     val accountNumberNotifier = EventNotifier<String?>(null)
@@ -37,9 +49,16 @@ class SettingsListener(endpoint: ServiceEndpoint) {
         settingsNotifier.subscribe(this) { settings ->
             endpoint.sendEvent(Event.SettingsUpdate(settings))
         }
+
+        endpoint.dispatcher.apply {
+            registerHandler(Request.SetWireGuardMtu::class) { request ->
+                commandChannel.sendBlocking(Command.SetWireGuardMtu(request.mtu))
+            }
+        }
     }
 
     fun onDestroy() {
+        commandChannel.close()
         daemon.unregisterListener(this)
 
         accountNumberNotifier.unsubscribeAll()
@@ -75,6 +94,20 @@ class SettingsListener(endpoint: ServiceEndpoint) {
             }
 
             settings = newSettings
+        }
+    }
+
+    private fun spawnActor() = GlobalScope.actor<Command>(Dispatchers.Default, Channel.UNLIMITED) {
+        try {
+            while (true) {
+                val command = channel.receive()
+
+                when (command) {
+                    is Command.SetWireGuardMtu -> daemon.await().setWireguardMtu(command.mtu)
+                }
+            }
+        } catch (exception: ClosedReceiveChannelException) {
+            // Closed sender, so stop the actor
         }
     }
 }
