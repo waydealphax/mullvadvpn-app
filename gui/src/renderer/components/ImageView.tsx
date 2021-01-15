ea9323fd2daac4be26abe554c4c396ee7c293cb0
Oskar Nyberg <oskar@mullvad.net>
2021-01-15 17:31:44 +0100

    Add support for data-URLs in ImageView

--- gui/src/renderer/components/ImageView.tsx
+++ gui/src/renderer/components/ImageView.tsx
@@ -39,9 +39,10 @@ const ImageMask = styled.div((props: IImageMaskProps) => {
 const HiddenImage = styled.img({ visibility: 'hidden' });
 
 export default function ImageView(props: IImageViewProps) {
-  const url = props.source.startsWith('data:')
-    ? props.source
-    : `../../assets/images/${props.source}.svg`;
+  const url =
+    path.isAbsolute(props.source) || props.source.startsWith('data:')
+      ? props.source
+      : `../../assets/images/${props.source}.svg`;
 
   // eslint-disable-next-line @typescript-eslint/naming-convention
   const style = useMemo(() => ({ WebkitMaskImage: `url('${url}')` }), [url]);
