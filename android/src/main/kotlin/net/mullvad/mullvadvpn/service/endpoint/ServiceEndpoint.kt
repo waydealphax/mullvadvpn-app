6aa0e6450dc9cb31aadf72adcd558b04de73446e
Janito Vaqueiro Ferreira Filho <janito@mullvad.net>
2021-01-26 01:02:38 +0000

    Wait for daemon to be ready before registering

--- android/src/main/kotlin/net/mullvad/mullvadvpn/service/endpoint/ServiceEndpoint.kt
+++ android/src/main/kotlin/net/mullvad/mullvadvpn/service/endpoint/ServiceEndpoint.kt
@@ -3,14 +3,21 @@ package net.mullvad.mullvadvpn.service.endpoint
 import android.os.DeadObjectException
 import android.os.Looper
 import android.os.Messenger
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.channels.ClosedReceiveChannelException
+import kotlinx.coroutines.channels.actor
+import kotlinx.coroutines.channels.sendBlocking
 import net.mullvad.mullvadvpn.ipc.DispatchingHandler
 import net.mullvad.mullvadvpn.ipc.Event
 import net.mullvad.mullvadvpn.ipc.Request
 import net.mullvad.mullvadvpn.service.MullvadDaemon
 import net.mullvad.mullvadvpn.util.Intermittent
 
-class ServiceEndpoint(looper: Looper, intermittentDaemon: Intermittent<MullvadDaemon>) {
+class ServiceEndpoint(looper: Looper, private val intermittentDaemon: Intermittent<MullvadDaemon>) {
     private val listeners = mutableListOf<Messenger>()
+    private val registrationQueue = startRegistrator()
 
     internal val dispatcher = DispatchingHandler(looper) { message ->
         Request.fromMessage(message)
@@ -22,12 +29,13 @@ class ServiceEndpoint(looper: Looper, intermittentDaemon: Intermittent<MullvadDa
 
     init {
         dispatcher.registerHandler(Request.RegisterListener::class) { request ->
-            registerListener(request.listener)
+            registrationQueue.sendBlocking(request.listener)
         }
     }
 
     fun onDestroy() {
         dispatcher.onDestroy()
+        registrationQueue.close()
         settingsListener.onDestroy()
     }
 
@@ -44,6 +52,23 @@ class ServiceEndpoint(looper: Looper, intermittentDaemon: Intermittent<MullvadDa
 
         for (deadListener in deadListeners) {
             listeners.remove(deadListener)
+        }
+    }
+
+    private fun startRegistrator() = GlobalScope.actor<Messenger>(
+        Dispatchers.Default,
+        Channel.UNLIMITED
+    ) {
+        try {
+            while (true) {
+                val listener = channel.receive()
+
+                intermittentDaemon.await()
+
+                registerListener(listener)
+            }
+        } catch (exception: ClosedReceiveChannelException) {
+            // Registration queue closed; stop registrator
         }
     }
 
