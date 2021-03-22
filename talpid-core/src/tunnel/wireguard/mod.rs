d25af1ee6189281f44b0f180a3882d01decb4c6c
David Lönnhager <david.l@mullvad.net>
2021-03-22 17:08:49 +0100

    Force caller to deal with default route callback handle

--- talpid-core/src/tunnel/wireguard/mod.rs
+++ talpid-core/src/tunnel/wireguard/mod.rs
@@ -57,6 +57,8 @@ pub struct WireguardMonitor {
     close_msg_sender: mpsc::Sender<CloseMsg>,
     close_msg_receiver: mpsc::Receiver<CloseMsg>,
     pinger_stop_sender: mpsc::Sender<()>,
+    #[cfg(target_os = "windows")]
+    _callback_handle: Option<crate::winnet::WinNetCallbackHandle>,
 }
 
 #[cfg(target_os = "linux")]
@@ -92,10 +94,14 @@ impl WireguardMonitor {
             .add_routes(Self::get_routes(&iface_name, &config))
             .map_err(Error::SetupRoutingError)?;
 
-        // TODO: Call winnet::add_default_route_change_callback directly and keep the handle?
         #[cfg(target_os = "windows")]
-        route_manager
-            .add_default_route_callback(Some(WgGoTunnel::default_route_changed_callback), ());
+        let callback_handle = route_manager
+            .add_default_route_callback(Some(WgGoTunnel::default_route_changed_callback), ())
+            .ok();
+        #[cfg(target_os = "windows")]
+        if callback_handle.is_none() {
+            log::warn!("Failed to register default route callback");
+        }
 
         let event_callback = Box::new(on_event.clone());
         let (close_msg_sender, close_msg_receiver) = mpsc::channel();
@@ -106,6 +112,8 @@ impl WireguardMonitor {
             close_msg_sender,
             close_msg_receiver,
             pinger_stop_sender: pinger_tx,
+            #[cfg(target_os = "windows")]
+            _callback_handle: callback_handle,
         };
 
         let metadata = Self::tunnel_metadata(&iface_name, &config);
